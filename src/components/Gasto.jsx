85915e8006a754ebda50acf10ff8b7bd1d23c15d
DABB22 <di34nd@gmail.com>
2023-05-10 17:36:44 -0500

    primer commit

---
+++ src/components/Gasto.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import {
+    LeadingActions,
+    SwipeableList,
+    SwipeableListItem,
+    SwipeAction,
+    TrailingActions
+} from 'react-swipeable-list'
+import "react-swipeable-list/dist/styles.css"
+
+import { formatearFecha } from '../helpers'
+
+import IconoAhorro from '../img/icono_ahorro.svg'
+import IconoCasa from '../img/icono_casa.svg'
+import IconoComida from '../img/icono_comida.svg'
+import IconoGastos from '../img/icono_gastos.svg'
+import IconoOcio from '../img/icono_ocio.svg'
+import IconoSalud from '../img/icono_salud.svg'
+import IconoSuscripciones from '../img/icono_suscripciones.svg'
+
+const diccionarioIconos = {
+    ahorro : IconoAhorro,
+    comida : IconoComida,
+    casa : IconoCasa,
+    gastos : IconoGastos,
+    ocio : IconoOcio,
+    salud : IconoSalud,
+    suscripciones : IconoSuscripciones
+}
+
+const Gasto = ({gasto, setGastoEditar, eliminarGasto}) => {
+    const { categoria, nombre, cantidad, id, fecha, fechaEdicion } = gasto;
+
+    const leadingActions = () => (//este parentesis quiere decir que es un return
+        <LeadingActions>
+            {/* este swipwAction es obligatorio pasarle un onClick */}
+            <SwipeAction onClick={() => setGastoEditar(gasto)}>
+                Editar
+            </SwipeAction>
+        </LeadingActions>
+    )
+
+    const trailingActions = () => ( //este parentesis quiere decir que es un return
+        <TrailingActions>
+            {/* este swipwAction es obligatorio pasarle un onClick */}
+            <SwipeAction 
+                onClick={() => eliminarGasto(id)}
+                //animación para cuando se elimina 
+                destructive={true}
+            >
+                Eliminar
+            </SwipeAction>
+        </TrailingActions>
+    )
+
+    return (
+        <SwipeableList>
+            <SwipeableListItem
+                leadingActions={leadingActions()}
+                trailingActions={trailingActions()}
+            >
+                <div className="gasto sombra">
+                    <div className="contenido-gasto">
+                        <img 
+                            src={diccionarioIconos[categoria]}
+                            alt="Icono Gasto"
+                        />
+                        <div className="descripcion-gasto">
+                            <p className="categoria">{categoria}</p>
+                            <p className="nombre-gasto">{nombre}</p>
+                            <p className="fecha-gasto">
+                                Agregado el: {''}
+                                <span>{formatearFecha(fecha)}</span>
+                            </p>
+                            { fechaEdicion && <p className="fecha-gasto">
+                                Última edición el: {''}
+                                <span>{formatearFecha(fechaEdicion)}</span>
+                            </p>
+                            }
+                        </div>
+                    </div>
+                    <p className="cantidad-gasto">${cantidad}</p>
+                </div>
+            </SwipeableListItem>
+        </SwipeableList>
+    )
+}
+
+export default Gasto
